108343a91bd0c900a09c5e858be8474fdea89c8c
Ronilson Batista <ronilson.batista@orama.com.br>
2020-04-29 12:19:06 -0300

    fix

--- OramaCadastroSwift/Classes/OpenAPIs/Models/DadosProfissionais.swift
+++ OramaCadastroSwift/Classes/OpenAPIs/Models/DadosProfissionais.swift
@@ -16,14 +16,14 @@ import Foundation
     /** Nome da empresa em que trabalha o usuário.  - É obrigatório caso o usuário esteja empregado. */
     public var empresa: String?
     /** Se o usuário esta empregado no momento. */
-    public var empregado: Bool = true
+    public var empregado: Bool? = true
     public var empregadoNum: NSNumber? {
         get {
             return empregado as NSNumber?
         }
     }
 
-    public init(profissao: String?, empresa: String?, empregado: Bool) {
+    public init(profissao: String?, empresa: String?, empregado: Bool?) {
         self.profissao = profissao
         self.empresa = empresa
         self.empregado = empregado
