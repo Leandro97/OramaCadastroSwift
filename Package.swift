a3b0d40c4e99e7c17fab3601185b221167e09e73
Eduardo Jr <eduardojr@Mac-mini-de-Eduardo.local>
2021-05-24 15:25:20 -0300

    fix

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OramaCadastroSwift",
-            dependencies: [],
+            dependencies: ["Alamofire"],
             path: "OramaCadastroSwift/Classes/OpenAPIs/")
     ],
     swiftLanguageVersions: [.v5]
