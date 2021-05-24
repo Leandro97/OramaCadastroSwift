11b675c0ba9ab4c38b29ac8667d0a211f969877a
Eduardo Jr <eduardojr@Mac-mini-de-Eduardo.local>
2021-05-24 14:49:19 -0300

    fix

--- Package.swift
+++ Package.swift
@@ -12,7 +12,10 @@ let package = Package(
             name: "OramaCadastroSwift",
             targets: ["OramaCadastroSwift"]),
     ],
-    dependencies: [],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.0.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
