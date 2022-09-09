54d79d7bbacb5ae51c1342a2a6bf43788a91a24e
Leandro Freitas <leandro.freitas@orama.com.br>
2022-09-09 09:21:29 -0300

    ARES-143 Updating base url

--- OramaCadastroSwift/Classes/OpenAPIs/APIs.swift
+++ OramaCadastroSwift/Classes/OpenAPIs/APIs.swift
@@ -8,7 +8,7 @@ import Foundation
 
 open class OramaCadastroSwiftAPI {
     #if DEBUG
-    public static var basePath = "https://ext01-cadastro.orama.com.br/api/contas/v3.0"
+    public static var basePath = "https://hml-cadastro.orama.com.br/api/contas/v3.0"
     #elseif RELEASE
     public static var basePath = "https://cadastro.orama.com.br/api/contas/v3.0"
     #endif
@@ -40,6 +40,7 @@ open class RequestBuilder<T> {
                 
         addHeaders(OramaCadastroSwiftAPI.customKeyHeaders)
         addHeaders(OramaCadastroSwiftAPI.customHeaders)
+        addHeaders(["X-Api-Key": "WxR4kiib.0epWsxPAWTlbLQ1iNIbpZlKNQzpoznal"])
     }
     
     open func addHeaders(_ aHeaders:[String:String]) {
