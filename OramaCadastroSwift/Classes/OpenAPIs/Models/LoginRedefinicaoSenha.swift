569aa7d9d98adde3dfc4c27adbc1465549682a43
Andre FIrmo <andre.firmo@orama.com.br>
2022-11-10 18:12:37 -0300

    Alterando o api para recer 204

--- OramaCadastroSwift/Classes/OpenAPIs/Models/LoginRedefinicaoSenha.swift
+++ OramaCadastroSwift/Classes/OpenAPIs/Models/LoginRedefinicaoSenha.swift
@@ -12,11 +12,11 @@ import Foundation
 @objc public class LoginRedefinicaoSenha: NSObject, Codable { 
 
     public var cpf: String?
-    public var email: String?
+//    public var email: String?
 
-    public init(cpf: String?, email: String?) {
+    public init(cpf: String?) {
         self.cpf = cpf
-        self.email = email
+//        self.email = email
     }
 
 }
